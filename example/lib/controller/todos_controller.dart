5d2728946bcf537bb11f5df99feafb47c74d2ba0
SwanFlutter1993 <swan.dev1993@gmail.com>
2025-02-07 08:18:02 -0800

    add document new features

--- example/lib/controller/todos_controller.dart
+++ example/lib/controller/todos_controller.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:getx/getx.dart';
 
 class TodosController extends GetxController {
   static TodosController get to => Get.find();
-  RxList<String> todos = <String>[].obs;
+
+  final ValueNotifier<ScrollNotification?> notifier = ValueNotifier(null);
+
+  final RxList<String> todos = <String>[].obs;
 }
--- example/lib/screen/home.dart
+++ example/lib/screen/home.dart
@@ -1,3 +1,4 @@
+import 'package:app_bar_auto_hide/app_bar_auto_hide.dart';
 import 'package:example/controller/todos_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:getx/getx.dart';
@@ -7,24 +8,32 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Todos'),
+      appBar: AppBarAutoHide(
+        notifier: TodosController.to.notifier,
+        title: Text('Todos', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.deepPurple,
       ),
-      body: SizedBox(
-        height: context.height,
-        width: context.width,
-        child: TodosController.to.todos.isEmpty
-            ? const Center(child: Text('No todos'))
-            : ListView.builder(
-                itemCount: TodosController.to.todos.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    child: ListTile(
-                      title: Text('Todo $index'),
-                    ),
-                  );
-                },
-              ),
+      body: NotificationListener<ScrollNotification>(
+        onNotification: (notification) {
+          TodosController.to.notifier.value = notification;
+          return true;
+        },
+        child: SizedBox(
+          height: context.height,
+          width: context.width,
+          child: TodosController.to.todos.isEmpty
+              ? const Center(child: Text('No todos'))
+              : ListView.builder(
+                  itemCount: TodosController.to.todos.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: ListTile(
+                        title: Text('Todo $index'),
+                      ),
+                    );
+                  },
+                ),
+        ),
       ),
     );
   }
